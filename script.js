814569f4cb97bf6e41562680f244acd301dc9a0b
BSIT3B-BUENSUCESO <acebuensuceso@gmail.com>
2024-11-28 15:55:03 +0800

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Show welcome message
-function showWelcomeMessage() {
-  alert("Welcome to Pitik Ni Eys Studio!");
-}
-
-// Handle navigation
-function navigateTo(section) {
-  console.log("Navigating to:", section);
-}
